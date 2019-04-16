34e83e3e3b74cfd817c6641c560d8c4c849a4924
benjamin <bnjmnsepulveda@gmail.com>
2019-04-16 18:09:59 -0400

    se agregan script con comandos linux

---
+++ comandos-linux.sh
@@ -0,0 +1,8 @@
+# ------------ SYSTEMA ------------------
+# version ubuntu linux
+lsb_release -a
+
+# ----------- PYTHON --------------------
+# crear ambiente virtual python
+python3 -m venv $DIRECTORIO_VIRTUAL_PYTHON
+source $DIRECTORIO_VIRTUAL_PYTHON/bin/activate
---
+++ comandos-postgres.sh
@@ -0,0 +1,6 @@
+
+# cambiar usuario a postgres para usar terminal con psql
+sudo -i -u postgres
+# ingresar consola postres sin cambiar de usuario
+sudo -u postgres psql
+
