0d7cb3d1b25a748e63eb321733a5c5ffdae6d665
Benjamin <bnjmnsepulveda@gmail.com>
2019-11-10 13:37:03 -0300

    Update comandos-linux.sh

--- comandos-linux.sh
+++ comandos-linux.sh
@@ -5,8 +5,13 @@ lsb_release -a
 scp /ruta/al/archivo-origen usuario@ordenador:/ruta/al/directorio-destino/
 #copiar archivo remoto a local
 scp usuario@ordenador:/ruta/al/archivo-origen /ruta/al/directorio-destino
+#crea un archivo con un peso especifico
+fallocate -l 10M /opt/file-example
+#ver peso de archivos
+du -sh *
 
 # ----------- PYTHON --------------------
 # crear ambiente virtual python
 python3 -m venv $DIRECTORIO_VIRTUAL_PYTHON
 source $DIRECTORIO_VIRTUAL_PYTHON/bin/activate
+
